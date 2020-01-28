32188ab86401289dec8cb63fff1f0ad23541c9d0
Mauricio <mamerlinux@gmail.com>
2020-01-28 01:25:36 -0300

    fix: get address by api

--- src/screens/cart/CartShipping/index.js
+++ src/screens/cart/CartShipping/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
-import { View, Dimensions, Text, TouchableHighlight, TouchableOpacity, ScrollView } from 'react-native'
+import { View, Text, TouchableHighlight, TouchableOpacity, ScrollView, ActivityIndicator } from 'react-native'
 import { PlabCardView } from '../../../components'
 import styles from './styles'
 import { getShippingAddress } from '../../../services/Api'
@@ -8,19 +8,21 @@ import { getShippingAddress } from '../../../services/Api'
 const CartShipping = props => {
     const orderAmount = 123.3
 
-    const [loading, setLoading] = useState(null)
+    const [loading, setLoading] = useState(true)
     const [total, setTotal] = useState(orderAmount)
     const [shippingType, setShippingType] = useState(null)
     const [shippingDeadLine, setShippingDeadLine] = useState(null)
     const [data, setData] = useState(null)
 
     useEffect(() => {
+        console.log("SLDLSKDLSDK CALL")
         setLoading(true)
 
         const getData = () => {
-            getShippingAddress(user).then(response => {
+            getShippingAddress({ id: 1 }).then(response => {
+                console.log("SLDLSKDLSDK CALL response " + JSON.stringify(response.data))
+                setData(response.data)
                 setLoading(false)
-                setData(response)
             }).catch(error => {
                 setLoading(false)
                 Alert.alert("Carrinho", "Error ao obter os endereços")
@@ -30,90 +32,95 @@ const CartShipping = props => {
     }, []);
 
     const updateShipping = item => {
-        const newData = { ...data }
+        const newData = [ ...data ]
         newData.forEach(shipping => {
             newData.selected = (shipping.id === item.id)
         })
 
         setData(newData)
-        setTotal(orderAmount + mock[index].price)
-        setShippingType(mock[index].title)
-        setShippingDeadLine(mock[index].deadLine)
+        setTotal(orderAmount + item.price)
+        setShippingType(item.type)
+        setShippingDeadLine(item.deadLine)
     }
 
     const addAddress = () => {
         this.props.navigation.navigate('CreateAddress')
     }
 
-    return (
-        <ScrollView>
-            <View styles={styles.container}>
-                <PlabCardView style={styles.cardViewContainerDetail}>
-                    <Text style={styles.shippingTitle}>Detalhe do pedido</Text>
-                    <View style={styles.infoDetailContainer}>
-                        <Text style={styles.buyTitleText}>Valor do pedido:</Text>
-                        <Text style={styles.infoDetailDesc}>R$ {orderAmount}</Text>
-                    </View>
-                    {(shippingType !== null) ?
-                        <View style={styles.infoDetailContainer}>
-                            <Text style={styles.buyTitleText}>Tipo de entrega:</Text>
-                            <Text style={styles.infoDetailDesc}>{shippingType}</Text>
-                        </View>
-                        : null
-                    }
-                    {(shippingDeadLine !== null) ?
-                        <View style={styles.infoDetailContainer}>
-                            <Text style={styles.buyTitleText}>Prazo de entrega:</Text>
-                            <Text style={styles.infoDetailDesc}>{shippingDeadLine}</Text>
-                        </View>
-                        : null
-                    }
-                    <View style={styles.infoDetailContainer}>
-                        <Text style={styles.buyTitleText}>Valor total:</Text>
-                        <Text style={styles.infoDetailDesc}>R$ {total}</Text>
-                    </View>
-                </PlabCardView>
-                <Text style={{ color: 'black', marginBottom: 6, paddingStart: 24 }}>Selecine um tipo de entrega:</Text>
-                {data.map(shipping => (
-                    <TouchableOpacity
-                        onPress={() => updateShipping(shipping)}
-                        activeOpacity={1}>
-                        <PlabCardView style={styles.cardViewContainer}>
-                            <View style={{ flex: 1, flexDirection: 'row' }}>
-                                <View style={{ width: "3%", backgroundColor: (shipping.selected) ? '#535f69' : '#FFF' }} />
-                                <View style={{ width: "97%" }}>
-                                    <View style={styles.buyInfo}>
-                                        <Text style={styles.buyDescText}>{shipping.title}</Text>
-                                    </View>
-                                    {(shipping.recipient !== null) ?
-                                        <View style={styles.buyInfo}>
-                                            <Text style={styles.buyDescText}>{shipping.recipient}</Text>
-                                        </View>
-                                        : null}
-                                    <View style={styles.buyInfo}>
-                                        <Text style={styles.buyDescText}>
-                                            {shipping.address}
-                                            {(shipping.complement !== null) ? " " + shipping.complement : null}
-                                        </Text>
-                                    </View>
-                                    <View style={styles.buyInfo}>
-                                        <Text style={styles.buyDescText}>{shipping.city}</Text>
-                                    </View>
-                                    <View style={styles.buyInfo}>
-                                        <Text style={styles.buyDescText}>{shipping.cep}</Text>
-                                    </View>
+    const render = () => {
+        if (loading) {
+            return (<ActivityIndicator size="large" animating={loading} />)
+        } else {
+            return (
+                <ScrollView>
+                    <View styles={styles.container}>
+                        <PlabCardView style={styles.cardViewContainerDetail}>
+                            <Text style={styles.shippingTitle}>Detalhe do pedido</Text>
+                            <View style={styles.infoDetailContainer}>
+                                <Text style={styles.buyTitleText}>Valor do pedido:</Text>
+                                <Text style={styles.infoDetailDesc}>R$ {orderAmount}</Text>
+                            </View>
+                            {(shippingType !== null) ?
+                                <View style={styles.infoDetailContainer}>
+                                    <Text style={styles.buyTitleText}>Tipo de entrega:</Text>
+                                    <Text style={styles.infoDetailDesc}>{shippingType}</Text>
                                 </View>
+                                : null
+                            }
+                            {(shippingDeadLine !== null) ?
+                                <View style={styles.infoDetailContainer}>
+                                    <Text style={styles.buyTitleText}>Prazo de entrega:</Text>
+                                    <Text style={styles.infoDetailDesc}>{shippingDeadLine}</Text>
+                                </View>
+                                : null
+                            }
+                            <View style={styles.infoDetailContainer}>
+                                <Text style={styles.buyTitleText}>Valor total:</Text>
+                                <Text style={styles.infoDetailDesc}>R$ {total}</Text>
                             </View>
                         </PlabCardView>
-                    </TouchableOpacity>
-                ))}
-            </View>
-            <TouchableHighlight onPress={() => addAddress()}>Adicionar endereço</TouchableHighlight>
-            <PlabButton
-                style={styles.button}
-                text="CONTINUAR"
-                onPress={() => this.save()} />
-        </ScrollView >
+                        <Text style={{ color: 'black', marginBottom: 6, paddingStart: 24 }}>Selecine um tipo de entrega:</Text>
+                        {data.map(shipping => (
+                            <TouchableOpacity
+                                onPress={() => updateShipping(shipping)}
+                                activeOpacity={1}>
+                                <PlabCardView style={styles.cardViewContainer}>
+                                    <View style={{ flex: 1, flexDirection: 'row' }}>
+                                        <View style={{ width: "3%", backgroundColor: (shipping.selected) ? '#535f69' : '#FFF' }} />
+                                        <View style={{ width: "97%" }}>
+                                            <View style={styles.buyInfo}>
+                                                <Text style={styles.buyDescText}>{shipping.type}</Text>
+                                            </View>
+                                            {(shipping.recipient !== null) ?
+                                                <View style={styles.buyInfo}>
+                                                    <Text style={styles.buyDescText}>{shipping.recipient}</Text>
+                                                </View>
+                                                : null}
+                                            <View style={styles.buyInfo}>
+                                                <Text style={styles.buyDescText}>
+                                                    {shipping.address}
+                                                    {(shipping.complement !== null) ? " " + shipping.complement : null}
+                                                </Text>
+                                            </View>
+                                            <View style={styles.buyInfo}>
+                                                <Text style={styles.buyDescText}>{shipping.city + " - " + shipping.state}</Text>
+                                            </View>
+                                            <View style={styles.buyInfo}>
+                                                <Text style={styles.buyDescText}>{shipping.cep}</Text>
+                                            </View>
+                                        </View>
+                                    </View>
+                                </PlabCardView>
+                            </TouchableOpacity>
+                        ))}
+                    </View>
+                </ScrollView >
+            )
+        }
+    }
+
+    return (
+        render()
     )
 }
 
--- src/services/Api.js
+++ src/services/Api.js
@@ -1,8 +1,9 @@
+import React from 'react'
 import Axios from 'axios'
 
 const axios = Axios.create({
-    baseURL: "http://ec2-54-173-117-10.compute-1.amazonaws.com:8080",
-    //baseURL: "http://192.168.0.7:8080",
+    //baseURL: "http://ec2-54-173-117-10.compute-1.amazonaws.com:8080",
+    baseURL: "http://192.168.0.7:8080/",
     responseType: "json"
 });
 
@@ -11,11 +12,11 @@ const url = "http://ec2-18-234-166-48.compute-1.amazonaws.com:8080"
 const login = user => axios.post("login/", user);
 const create = user => axios.post("user/", user);
 const getShipping = cep => axios.get("shipping/", { cep: cep });
-const getShippingAddress = user => axios.get("shipping/address");
+const getShippingAddress = user => axios.get("shipping/address/user/" + user.id);
 const createOrder = user => axios.post("order/", { user: user.id });
 const upload = (order, user, photo) => axios.post("photo/upload", { user: user.id, order: order.id, photo: photo });
 const updateOrderToSaved = (user, order) => axios.put("order/", { order: order.id, user: user.id });
-const getLastOrderCreated = user => axios.get("/last/order/created", { user: user.id });
+const getLastOrderCreated = user => axios.get("last/order/created", { user: user.id });
 const createAddress = address => axios.post("address/", address)
 
 export {
